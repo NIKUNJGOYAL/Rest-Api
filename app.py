85577ad45992820d6b680fd13f775b0520a3f342
Nikunj goyal <nikunjgoyal@Nikunjs-MacBook-Air.local>
2020-05-16 16:11:25 +0530

    fix heroku

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from resources.item import Item,ItemList
 from resources.store import Store,StoreList
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE URI']=os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE URI']='postgres://qvobgdqliizdqh:320b5dbebc0df4ed0fd90434abbfbcca488569479c0df42ba74ac0cbf1d49043@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d8n6h3ekfp2jne'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='jose'
 api=Api(app)
